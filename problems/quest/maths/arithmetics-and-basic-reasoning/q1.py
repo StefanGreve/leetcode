28ba41d3261ab4eb2d1bc488623907684c68a7d5
StefanGreve <greve.stefan@outlook.jp>
2025-12-14 20:15:40 +0100

    Update comments

--- problems/quest/maths/arithmetics-and-basic-reasoning/q1.py
+++ problems/quest/maths/arithmetics-and-basic-reasoning/q1.py
@@ -8,6 +8,10 @@ from typing import List
 # between any two consecutive elements is the same.
 # Given an array of numbers arr, return true if the array can be rearranged
 # to form an arithmetic progression. Otherwise, return false.
+#
+# CONSTRAINTS
+# 2 <= arr.length <= 1000
+# -10^6 <= arr[i] <= 10^6
 
 ExampleData = namedtuple("Example", ["input", "output"])
 
