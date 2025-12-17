9bfeffcb8c6ea5fdb018823351bafd37cbde3764
StefanGreve <greve.stefan@outlook.jp>
2025-12-17 19:27:42 +0100

    Add solution for Q3 (Palindrome Number)

---
+++ problems/quest/maths/arithmetics-and-basic-reasoning/q3.py
@@ -0,0 +1,48 @@
+import random
+from collections import namedtuple
+
+# PROBLEM STATEMENT
+# https://leetcode.com/problems/palindrome-number/?envType=problem-list-v2&envId=maths-m1-arithmetic-basic-reasoning
+#
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+#
+# CONSTRAINTS
+# -2^{31} <= x <= 2^{31}-1
+#
+# EXAMPLE
+# x=121 is a palindrome, because 121 reads as 121 from left to right and from right to left.
+# x=-121 is not a palindrome, because from left to right, it reads -121, and From right to left, it becomes 121-.
+# x=10 is not a palindrome, because it reads 01 from right to left.
+
+ExampleData = namedtuple("ExampleData", ["input", "output"])
+
+def isPalindrome(x: int) -> bool:
+    # Negative numbers are never a palindrome
+    if (x < 0): return False
+
+    # Single-digit numbers are palindromes
+    if (x < 10): return True
+
+    rev: int = 0
+    tmp: int = x
+
+    while (tmp != 0):
+        # Shift left once (REV * 10),
+        # then add the last number (TMP % 10).
+        rev = (rev * 10) + (tmp % 10)
+        # Pop the last digit from TMP
+        tmp //= 10
+
+    return rev == x
+
+if __name__ == '__main__':
+    results = [
+        ExampleData(input=x, output=isPalindrome(x)) for x in (
+            random.randint(-2**31, 2**31) for _ in range(100_000)
+        )
+    ]
+
+    print("\n".join(map(str, results)))
+
+
+
